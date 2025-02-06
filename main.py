4a3e6fbe01ef637c3f052829268839c109a3c183
Pedro Ratti <pedrohratti@gmail.com>
2025-02-06 17:49:50 -0300

    cors implemented

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends, HTTPException, Query
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from database import get_db
 from models import Imovel
@@ -6,6 +7,15 @@ from schemas import ImovelSchema
 from typing import List, Optional
 
 app = FastAPI()
+
+# Configurando CORS para rodar localmente
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Listar todas as acomodações
 @app.get("/acomodacoes", response_model=List[ImovelSchema])
